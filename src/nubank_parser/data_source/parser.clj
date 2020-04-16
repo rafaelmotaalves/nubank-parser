34d08617e699d3edf27f6b7c734a6bc748ed2a1f
Rafael Mota <rma7@cin.ufpe.br>
2020-04-16 16:39:58 -0300

    Add unit tests and improve data parsing

--- src/nubank_parser/data_source/parser.clj
+++ src/nubank_parser/data_source/parser.clj
@@ -11,7 +11,7 @@
 
 (defn- convert-data-types [entry]
   "Converts the :amount field to number and the :date field to a clj-time object"
-  (update (update entry :amount read-string) :date c/from-string))
+  (update (update (update entry :amount read-string) :date c/from-string) :category #(if (empty? %) nil %)))
 
 (defn- get-header-as-keywords [header]
   "Converts the header string into a list of keywords"
@@ -21,3 +21,4 @@
   "Parses a data source, returning a list of structured maps"
   (let [header (get-header-as-keywords (first lines))]
     (map (comp convert-data-types (partial parse-csv-line header)) (drop 1 lines))))
+ 
