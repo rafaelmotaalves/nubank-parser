a04d6f4b3e7c333ae4e8ea1bfe9d90436fecfb1c
Rafael Mota <rma7@cin.ufpe.br>
2020-05-24 21:02:52 -0300

    Make the group-by optional

--- src/nubank_parser/core.clj
+++ src/nubank_parser/core.clj
@@ -1,11 +1,10 @@
 (ns nubank-parser.core
   (:gen-class)
-  (:require 
-      [nubank-parser.data-source.parser :as parser]
-      [nubank-parser.data-source.reader :as reader]
-      [nubank-parser.transformations.group-by :as group-by]
-      [nubank-parser.presenters.simple-table :as simple-table]
-      ))
+  (:require
+   [nubank-parser.data-source.parser :as parser]
+   [nubank-parser.data-source.reader :as reader]
+   [nubank-parser.transformations.group-by :as group-by]
+   [nubank-parser.presenters.simple-table :as simple-table]))
 
 (defn get-credit-card-entries [directory-path]
   (flatten (map parser/parse (reader/read-entries directory-path))))
@@ -13,11 +12,13 @@
 (defn execute
   "Reads a list of csv files and prints the corresponding maps"
   [directory-path options]
-  (let [{group-str :group-by} options]
+  (let [{group-column :group-by} options]
     (let [credit-card-entries (get-credit-card-entries directory-path)]
-      (doseq [[month entries] (group-by (group-by/comp-funcs (keyword group-str)) credit-card-entries)]
-        (simple-table/print-output month entries)
-        ))
-    )
-  )
+      (if (nil? group-column)
+        (simple-table/print-table credit-card-entries)
+        (doseq [[group entries] (group-by (group-by/comp-funcs group-column) credit-card-entries)]
+          (simple-table/print-header group)
+          (simple-table/print-table entries)
+
+          )))))
 
--- src/nubank_parser/presenters/simple_table.clj
+++ src/nubank_parser/presenters/simple_table.clj
@@ -1,12 +1,10 @@
 (ns nubank-parser.presenters.simple-table
   (:gen-class)
   (:require
-    [clojure.pprint :as pprint]
-    ))
+   [clojure.pprint :as pprint]))
 
-(defn print-output [header entries]
-    (do
-        (println (clojure.string/join " - " header))
-        (pprint/print-table entries)
-    )
-)
+(defn print-header [header]
+  (println (clojure.string/join " - " header)))
+
+(defn print-table [entries]
+  (pprint/print-table entries))
