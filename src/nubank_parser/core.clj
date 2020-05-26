f836981058d4f0a9e0dc102030df9ab3e7fa3536
Rafael Mota <rma7@cin.ufpe.br>
2020-05-25 22:09:23 -0300

    Refactor core method

--- src/nubank_parser/core.clj
+++ src/nubank_parser/core.clj
@@ -5,11 +5,15 @@
    [nubank-parser.data-source.reader :as reader]
    [nubank-parser.transformations.group-by :as group-by]
    [nubank-parser.presenters.simple-table :as simple-table]
-   [nubank-parser.transformations.aggregate :refer [aggregate]]
-   ))
+   [nubank-parser.transformations.aggregate :refer [aggregate]]))
 
 (defn get-credit-card-entries [directory-path]
   (flatten (map parser/parse (reader/read-entries directory-path))))
+
+(defn group-by-aggregate [rows aggregate-method group-column]
+  (let [groups (group-by #(select-keys % [group-column]) rows)]
+    (let [result (reduce-kv (fn [arr key value] (conj arr (assoc key aggregate-method (aggregate aggregate-method value)))) [] groups)]
+      (simple-table/print-table result))))
 
 (defn execute
   "Reads a list of csv files and prints the corresponding maps"
@@ -20,13 +24,9 @@
     (let [credit-card-entries (filter where-fn (get-credit-card-entries directory-path))]
       (cond
         (empty? credit-card-entries) (println "No entries or csv files found on input directory")
-        (and (not (nil? group-column)) (not (nil? aggregate-method)))
-        (let [groups (group-by #(select-keys % [group-column]) credit-card-entries)]
-          (let [result (reduce-kv (fn [arr key value] (conj arr (assoc key aggregate-method (aggregate aggregate-method value)))) [] groups)]
-            (simple-table/print-table result)
-          )
-        )
-        (not (nil? group-column)) (println "If you provide the group-by option you must also provide the aggregate")
-        (not (nil? aggregate-method)) (println (aggregate aggregate-method credit-card-entries))
+        (and group-column aggregate-method)
+        (group-by-aggregate credit-card-entries aggregate-method group-column)
+        (group-column) (println "If you provide the group-by option you must also provide the aggregate")
+        (aggregate-method) (println (aggregate aggregate-method credit-card-entries))
         :else (simple-table/print-table credit-card-entries)))))
 
