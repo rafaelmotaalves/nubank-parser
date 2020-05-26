04852c995e7369b1618e4574f57731a556ec162a
Rafael Mota <rma7@cin.ufpe.br>
2020-05-25 23:25:42 -0300

    Refactor core/execute

--- src/nubank_parser/core.clj
+++ src/nubank_parser/core.clj
@@ -1,27 +1,25 @@
 (ns nubank-parser.core
   (:gen-class)
   (:require
-   [nubank-parser.data-source.parser :as parser]
-   [nubank-parser.data-source.reader :as reader]
-   [nubank-parser.transformations.group-by :refer [group-by-aggregate]]
-   [nubank-parser.presenters.simple-table :as simple-table]
-   [nubank-parser.transformations.aggregate :refer [aggregate]]))
+   [nubank-parser.data-source.parser :as data-source.parser]
+   [nubank-parser.data-source.reader :as data-source.reader]
+   [nubank-parser.transformations.aggregate :as transformations.aggregate]
+   [nubank-parser.transformations.group-by :as transformations.group-by]
+   [nubank-parser.presenters.simple-table :as simple-table]))
 
-(defn get-credit-card-entries [directory-path]
-  (flatten (map parser/parse (reader/read-entries directory-path))))
+(defn get-credit-card-entries 
+  [directory-path]
+  (flatten (map data-source.parser/parse (data-source.reader/read-entries directory-path))))
 
 (defn execute
   "Reads a list of csv files and prints the corresponding maps"
   [directory-path options]
-  (let [{group-column :group-by} options
-        {where-fn :where} options
-        {aggregate-method :aggregate} options]
-    (let [credit-card-entries (filter where-fn (get-credit-card-entries directory-path))]
-      (cond
-        (empty? credit-card-entries) (println "No entries or csv files found on input director wy")
-        (and group-column aggregate-method)
-        (simple-table/print-table (group-by-aggregate credit-card-entries aggregate-method group-column))
-        (group-column) (println "If you provide the group-by option you must also provide the aggregate")
-        (aggregate-method) (println (aggregate aggregate-method credit-card-entries))
-        :else (simple-table/print-table credit-card-entries)))))
-
+  (let [{:keys [group-by aggregate where]} options]
+    (let [credit-card-entries (get-credit-card-entries directory-path)]
+      (-> credit-card-entries
+          (#(filter where %))
+          (#(cond
+              (and aggregate group-by) (transformations.group-by/group-by-aggregate % aggregate group-by)
+              aggregate [(assoc {} aggregate (transformations.aggregate/aggregate % aggregate))]
+              :else %))
+          (#(simple-table/print-table %))))))
